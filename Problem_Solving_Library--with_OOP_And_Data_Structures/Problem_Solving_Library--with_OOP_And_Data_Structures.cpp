f30e69246048642c1ca420f5383de2256bf0b087
Bourouail-Issam <issambourouail33@gmail.com>
2024-12-01 23:20:34 +0100

    Implement and test clsMyQueue
    - Added `clsMyQueue` with methods: `push`, `pop`, `front`, `back`, `Size`, `IsEmpty`, `Print`.
    - Tested in `main` with basic queue operations.

--- Problem_Solving_Library--with_OOP_And_Data_Structures/Problem_Solving_Library--with_OOP_And_Data_Structures.cpp
+++ Problem_Solving_Library--with_OOP_And_Data_Structures/Problem_Solving_Library--with_OOP_And_Data_Structures.cpp
@@ -1,58 +1,58 @@
 #include <iostream>
 #include "clsDblLinkedList.h"
-
+#include "clsMyQueue.h"
 using namespace std;
 
 int main()
 {
-	clsDblLinkedList <int> MydblLinkedList;
+	//clsDblLinkedList <int> MydblLinkedList;
 
-	MydblLinkedList.InsertAtBeginning(410);
-	MydblLinkedList.InsertAtBeginning(9);
-	MydblLinkedList.InsertAtBeginning(8);
-	MydblLinkedList.InsertAtBeginning(7);
-	MydblLinkedList.InsertAtBeginning(6);
-	MydblLinkedList.InsertAtBeginning(5);
-	MydblLinkedList.InsertAtBeginning(4);
-	MydblLinkedList.InsertAtBeginning(3);
-	MydblLinkedList.InsertAtBeginning(2);
-	MydblLinkedList.InsertAtBeginning(1);
-	cout << "\nDouble Linked List Content :\n";
-	MydblLinkedList.PrintList();
+	//MydblLinkedList.InsertAtBeginning(410);
+	//MydblLinkedList.InsertAtBeginning(9);
+	//MydblLinkedList.InsertAtBeginning(8);
+	//MydblLinkedList.InsertAtBeginning(7);
+	//MydblLinkedList.InsertAtBeginning(6);
+	//MydblLinkedList.InsertAtBeginning(5);
+	//MydblLinkedList.InsertAtBeginning(4);
+	//MydblLinkedList.InsertAtBeginning(3);
+	//MydblLinkedList.InsertAtBeginning(2);
+	//MydblLinkedList.InsertAtBeginning(1);
+	//cout << "\nDouble Linked List Content :\n";
+	//MydblLinkedList.PrintList();
 
-	clsDblLinkedList<int>::Node* N1 = MydblLinkedList.Find(2);
-	if (N1 != NULL)
-		cout << "\nNode with value 2 is Found :-)\n";
-	else
-		cout << "\nNode Is Not Found :-(\n";
+	//clsDblLinkedList<int>::Node* N1 = MydblLinkedList.Find(2);
+	//if (N1 != NULL)
+	//	cout << "\nNode with value 2 is Found :-)\n";
+	//else
+	//	cout << "\nNode Is Not Found :-(\n";
 
-	MydblLinkedList.InsertAfter(N1, 555);
-	cout << "\nAfter Inserting 555 after 2 : \n";
-	MydblLinkedList.PrintList();
+	//MydblLinkedList.InsertAfter(N1, 555);
+	//cout << "\nAfter Inserting 555 after 2 : \n";
+	//MydblLinkedList.PrintList();
 
-	MydblLinkedList.InsertAtEnd(700);
-	cout << "\nAfter Inserting 700 At End : \n";
-	MydblLinkedList.PrintList();
+	//MydblLinkedList.InsertAtEnd(700);
+	//cout << "\nAfter Inserting 700 At End : \n";
+	//MydblLinkedList.PrintList();
 
-	clsDblLinkedList<int>::Node* N2 = MydblLinkedList.Find(4);
-	MydblLinkedList.DeleteNode(N2);
-	cout << "\nAfter Deleting 4 :\n";
-	MydblLinkedList.PrintList();
+	//clsDblLinkedList<int>::Node* N2 = MydblLinkedList.Find(4);
+	//MydblLinkedList.DeleteNode(N2);
+	//cout << "\nAfter Deleting 4 :\n";
+	//MydblLinkedList.PrintList();
 
-	MydblLinkedList.DeleteFirstNode();
-	cout << "\nAfter deleting first node :\n";
-	MydblLinkedList.PrintList();
+	//MydblLinkedList.DeleteFirstNode();
+	//cout << "\nAfter deleting first node :\n";
+	//MydblLinkedList.PrintList();
 
-	MydblLinkedList.DeleteLastNode();
-	cout << "\nAfter deleting last node :\n";
-	MydblLinkedList.PrintList();
+	//MydblLinkedList.DeleteLastNode();
+	//cout << "\nAfter deleting last node :\n";
+	//MydblLinkedList.PrintList();
+	//
+	//cout << "\nNumber of items in the linked list = " << MydblLinkedList.Size();
 
-	cout << "\nNumber of items in the linked list = " << MydblLinkedList.Size();
-
-	if (MydblLinkedList.IsEmpty())
-		cout << "\n\nYes List is Empty.\n";
-	else
-		cout << "\n\nNo List is not Empty.\n";
+	//if (MydblLinkedList.IsEmpty())
+	//	cout << "\n\nYes List is Empty.\n";
+	//else
+	//	cout << "\n\nNo List is not Empty.\n";
 
 
 	//cout << "\nExecuting .Clear()";
@@ -61,7 +61,7 @@ int main()
 	//cout << "\nNumber of items in the linked list = " << MydblLinkedList.Size() << endl;
 
 
-	MydblLinkedList.Reverse();
+	/*MydblLinkedList.Reverse();
 	cout << "\nDouble Linked List Content After Reversing  : ";
 	MydblLinkedList.PrintList();
 
@@ -87,5 +87,24 @@ int main()
 
 	MydblLinkedList.InsertAfter(1, 500);
 	cout << "\nAfter Insert : " << endl;
-	MydblLinkedList.PrintList();
+	MydblLinkedList.PrintList();*/
+
+	clsMyQueue <int> MyQueue;
+
+	MyQueue.push(10);
+	MyQueue.push(20);
+	MyQueue.push(30);
+	MyQueue.push(40);
+	MyQueue.push(50);
+
+	cout << "\nQueue : \n";
+	MyQueue.Print();
+
+	cout << "\nQueue Size : " << MyQueue.Size();
+	cout << "\nQueue front : " << MyQueue.front();
+	cout << "\nQueue back : " << MyQueue.back();
+
+	MyQueue.pop();
+	cout << "\n\nQueue after pop() : \n";
+	MyQueue.Print();
 }
