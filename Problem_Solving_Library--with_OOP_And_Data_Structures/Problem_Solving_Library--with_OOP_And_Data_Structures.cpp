4a2632184faf970bf43564dbd739d8859296e06c
Bourouail-Issam <issambourouail33@gmail.com>
2024-12-02 03:36:36 +0100

    Extend clsMyQueue functionality
    - Added methods: `GetTtem`, `Reverse`, `UpDateItem`, `InserAfter`, `InsertAtFront`, `InsertAtBack`, and `Clear`.
    - Updated `main` to demonstrate these new features.

--- Problem_Solving_Library--with_OOP_And_Data_Structures/Problem_Solving_Library--with_OOP_And_Data_Structures.cpp
+++ Problem_Solving_Library--with_OOP_And_Data_Structures/Problem_Solving_Library--with_OOP_And_Data_Structures.cpp
@@ -107,4 +107,37 @@ int main()
 	MyQueue.pop();
 	cout << "\n\nQueue after pop() : \n";
 	MyQueue.Print();
+
+	// Extension #1
+	cout << "\n\nItem(2) : " << MyQueue.GetTtem(2);
+
+	// Extension #2
+	MyQueue.Reverse();
+	cout << "\n\nQueue after reverse() : \n";
+	MyQueue.Print();
+
+	// Extension #3
+	MyQueue.UpDateItem(2, 600);
+	cout << "\n\nQueue after Updating Item(2) to 600 : \n";
+	MyQueue.Print();
+
+	// Extension #4
+	MyQueue.InserAfter(2, 800);
+	cout << "\n\nQueue after Inserting 800 after Item(2) : \n";
+	MyQueue.Print();
+
+	// Extension #5
+	MyQueue.InsertAtFront(1000);
+	cout << "\n\nQueue after Inserting 1000 at front : \n";
+	MyQueue.Print();
+
+	// Extension #6
+	MyQueue.InsertAtBack(2000);
+	cout << "\n\nQueue after Inserting 2000 at back : \n";
+	MyQueue.Print();
+
+	// Extension #6
+	MyQueue.Clear();
+	cout << "\n\nQueue after Clear() : \n";
+	MyQueue.Print();
 }
--- Problem_Solving_Library--with_OOP_And_Data_Structures/clsMyQueue.h
+++ Problem_Solving_Library--with_OOP_And_Data_Structures/clsMyQueue.h
@@ -39,5 +39,33 @@ public:
 		return _MyList.GetItem(Size() - 1);
 	}
 
+	T GetTtem(int index)
+	{
+		return _MyList.GetItem(index);
+	}
+	void Reverse()
+	{
+		_MyList.Reverse();
+	}
+	void UpDateItem(int index, T NewValue)
+	{
+		_MyList.UpdateItem(index, NewValue);
+	}
+	void InserAfter(int index, T NewValue)
+	{
+		_MyList.InsertAfter(index, NewValue);
+	}
+	void InsertAtFront(T NewValue)
+	{
+		_MyList.InsertAtBeginning(NewValue);
+	}
+	void InsertAtBack(T NewValue)
+	{
+		_MyList.InsertAtEnd(NewValue);
+	}
+	void Clear()
+	{
+		_MyList.Clear();
+	}
 };
 
