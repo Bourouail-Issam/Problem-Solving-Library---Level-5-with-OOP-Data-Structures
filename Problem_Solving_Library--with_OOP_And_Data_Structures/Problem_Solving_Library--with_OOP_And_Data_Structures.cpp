693446799c064efb2faaef92261d4c06706268c2
Bourouail-Issam <issambourouail33@gmail.com>
2024-12-04 03:00:04 +0100

    Add management methods to clsDynamicArray
    - Added new methods: `DeleteItemAt`, `DeleteFirstItem`, `DeleteLastItem`, `Find`, and `DeleteItem`.
    - Enhanced `main` to test these methods with various scenarios.
    - Improved functionality for dynamic array management and item deletion.

--- Problem_Solving_Library--with_OOP_And_Data_Structures/Problem_Solving_Library--with_OOP_And_Data_Structures.cpp
+++ Problem_Solving_Library--with_OOP_And_Data_Structures/Problem_Solving_Library--with_OOP_And_Data_Structures.cpp
@@ -145,58 +145,58 @@ int main()
 	//cout << "\n\nQueue after Clear() : \n";
 	//MyQueue.Print();
 
-//
-//clsMyStack <int> MyStack;
-//
-//MyStack.push(10);
-//MyStack.push(20);
-//MyStack.push(30);
-//MyStack.push(40);
-//MyStack.push(50);
-//
-//cout << "\nStack : \n";
-//MyStack.Print();
-//
-//cout << "\nStack Size : " << MyStack.Size();
-//cout << "\nStack Top : " << MyStack.Top();
-//cout << "\nStack Bottom : " << MyStack.Bottom();
-//
-//MyStack.pop();
-//cout << "\n\nStack after pop() : \n";
-//MyStack.Print();
-//
-//// Extension #1
-//cout << "\n\nItem(2) : " << MyStack.GetTtem(2);
-//
-//// Extension #2
-//MyStack.Reverse();
-//cout << "\n\nStack after reverse() : \n";
-//MyStack.Print();
-//
-//// Extension #3
-//MyStack.UpDateItem(2, 600);
-//cout << "\n\nStack after Updating Item(2) to 600 : \n";
-//MyStack.Print();
-//
-//// Extension #4
-//MyStack.InserAfter(2, 800);
-//cout << "\n\nStack after Inserting 800 after Item(2) : \n";
-//MyStack.Print();
-//
-//// Extension #5
-//MyStack.InsertAtFront(1000);
-//cout << "\n\nStack after Inserting 1000 at front : \n";
-//MyStack.Print();
-//
-//// Extension #6
-//MyStack.InsertAtBack(2000);
-//cout << "\n\nStack after Inserting 2000 at back : \n";
-//MyStack.Print();
-//
-//// Extension #6
-//MyStack.Clear();
-//cout << "\n\nStack after Clear() : \n";
-//MyStack.Print();
+	//
+    //clsMyStack <int> MyStack;
+    //
+    //MyStack.push(10);
+    //MyStack.push(20);
+    //MyStack.push(30);
+    //MyStack.push(40);
+    //MyStack.push(50);
+    //
+    //cout << "\nStack : \n";
+    //MyStack.Print();
+    //
+    //cout << "\nStack Size : " << MyStack.Size();
+    //cout << "\nStack Top : " << MyStack.Top();
+    //cout << "\nStack Bottom : " << MyStack.Bottom();
+    //
+    //MyStack.pop();
+    //cout << "\n\nStack after pop() : \n";
+    //MyStack.Print();
+    //
+    //// Extension #1
+    //cout << "\n\nItem(2) : " << MyStack.GetTtem(2);
+    //
+    //// Extension #2
+    //MyStack.Reverse();
+    //cout << "\n\nStack after reverse() : \n";
+    //MyStack.Print();
+    //
+    //// Extension #3
+    //MyStack.UpDateItem(2, 600);
+    //cout << "\n\nStack after Updating Item(2) to 600 : \n";
+    //MyStack.Print();
+    //
+    //// Extension #4
+    //MyStack.InserAfter(2, 800);
+    //cout << "\n\nStack after Inserting 800 after Item(2) : \n";
+    //MyStack.Print();
+    //
+    //// Extension #5
+    //MyStack.InsertAtFront(1000);
+    //cout << "\n\nStack after Inserting 1000 at front : \n";
+    //MyStack.Print();
+    //
+    //// Extension #6
+    //MyStack.InsertAtBack(2000);
+    //cout << "\n\nStack after Inserting 2000 at back : \n";
+    //MyStack.Print();
+    //
+    //// Extension #6
+    //MyStack.Clear();
+    //cout << "\n\nStack after Clear() : \n";
+    //MyStack.Print();
 
 	clsDynamicArray <int> MyDynamicArray(5);
 
@@ -216,22 +216,48 @@ int main()
 	//cout << "\nArray Size : " << MyDynamicArray.Size();
 	//cout << "\nArray Items after resize to 2 : \n";
 	//MyDynamicArray.PrintList();\
-	//
+    //
 	//MyDynamicArray.Resize(10);
 	//cout << "\nArray Size : " << MyDynamicArray.Size();
 	//cout << "\nArray Items after resize to 10 : \n";
 	//MyDynamicArray.PrintList();
 
-	cout << "\nItem(2) : " << MyDynamicArray.GetItem(-3) << endl;
+	cout << "\n\nItem(2) : " << MyDynamicArray.GetItem(2) << endl;
 
-	MyDynamicArray.Reverse();
+	//MyDynamicArray.Reverse();
+	//cout << "\nArray Items after reverse : \n";
+	//MyDynamicArray.PrintList();
 
-	cout << "\nArray Items after reverse : \n";
+	//MyDynamicArray.Clear();
+	//cout << "\nArray Items after Clear : \n";
+	//MyDynamicArray.PrintList();
+
+	MyDynamicArray.DeleteItemAt(2);
+	cout << "\n\nArray Items after deleting item(2) : ";
+	cout << "\nArray Size : " << MyDynamicArray.Size() << endl;
 	MyDynamicArray.PrintList();
 
-	MyDynamicArray.Clear();
-	cout << "\nArray Items after Clear : \n";
+	MyDynamicArray.DeleteFirstItem();
+	cout << "\n\nArray Items after deleting FirstItem : ";
+	cout << "\nArray Size : " << MyDynamicArray.Size() << endl;
 	MyDynamicArray.PrintList();
 
-	system("pause>0");
+	MyDynamicArray.DeleteLastItem();
+	cout << "\n\nArray Items after deleting LastItem : ";
+	cout << "\nArray Size : " << MyDynamicArray.Size() << endl;
+	MyDynamicArray.PrintList();
+
+	int Index = MyDynamicArray.Find(30);
+
+	if (-1 == Index)
+		cout << "\nItem was not Found : -(\n";
+	else
+		cout << "\n30 is Found at index : " << Index;
+
+	MyDynamicArray.DeleteItem(30);
+	cout << "\n\nArray Size : " << MyDynamicArray.Size();
+	cout << "\nArray Items after deleting 30 : ";
+	MyDynamicArray.PrintList();
+
+	//system("pause>0");
 }
--- Problem_Solving_Library--with_OOP_And_Data_Structures/clsDynamicArray.h
+++ Problem_Solving_Library--with_OOP_And_Data_Structures/clsDynamicArray.h
@@ -9,7 +9,7 @@ private:
 public:
 	T* OriginalArray;
 
-	clsDynamicArray(int Size = 0)
+	clsDynamicArray(int Size = 0) : _TempArray(nullptr)
 	{
 		if (Size < 0)
 			Size = 0;
@@ -49,6 +49,7 @@ public:
 		{
 			cout << OriginalArray[i] << " ";
 		}
+		cout << endl;
 	}
 
 	void Resize(int NewSize)
@@ -93,7 +94,6 @@ public:
 			{
 				_TempArray[_Size - 1 - i] = OriginalArray[i];
 			}
-
 			delete[] OriginalArray;
 			OriginalArray = _TempArray;
 		}
@@ -108,5 +108,89 @@ public:
 		OriginalArray = _TempArray;
 	}
 
+	// solution 1 for delete Item with index 
+	//bool DeleteItemAt(int index)
+	//{
+	//	if (_Size <= index || index < 0) return false;
+	//	
+	//	--_Size;
+	//	_TempArray = new T[_Size];
+	//	for (int i = 0; i < index; i++)
+	//	{
+	//		_TempArray[i] == OriginalArray[i];
+	//	}
+	//	for (int i = index+1; i < _Size; i++)
+	//	{
+	//		_TempArray[i - 1] = OriginalArray[1];
+	//	}
+	//	delete[] OriginalArray;
+	//	OriginalArray = _TempArray;
+	//	return true;
+	//}
+
+	/*The second function for delete Item with index is better because it is :
+	# More clear and easier to read.
+	# Uses only one loop, making it more efficient.
+	# Simplifies the copying process without the need for additional calculations.
+	*/
+	bool DeleteItemAt(int index)
+	{
+		if (_Size > index && index >= 0)
+		{
+			_TempArray = new T[_Size - 1];
+			int counter = 0;
+			for (int i = 0; i < _Size; i++)
+			{
+				if (index == i) continue;
+				_TempArray[counter] = OriginalArray[i];
+				++counter;
+			}
+			delete[] OriginalArray;
+			OriginalArray = _TempArray;
+			--_Size;
+			return true;
+		}
+		return false;
+	}
+
+	bool DeleteFirstItem()
+	{
+		if (_Size > 0)
+		{
+			DeleteItemAt(0);
+			return true;
+		}
+		return false;
+	}
+	bool DeleteLastItem()
+	{
+		if (_Size > 0)
+		{
+			DeleteItemAt(_Size - 1);
+			return true;
+		}
+		return false;
+	}
+
+	int Find(T value)
+	{
+		for (int i = 0; i < _Size; i++)
+		{
+			if (value == OriginalArray[i])
+				return i;
+		}
+		return -1;
+	}
+
+	bool DeleteItem(T value)
+	{
+		int index = Find(value);
+		if (index != -1)
+		{
+			DeleteItemAt(index);
+			return true;
+		}
+		return false;
+	}
 };
 
